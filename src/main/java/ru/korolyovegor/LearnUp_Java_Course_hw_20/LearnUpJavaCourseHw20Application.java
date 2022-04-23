799cdd207ea863bb05194e249e9322766990b693
KorolevEgor <korolevegir@gmail.com>
2022-04-23 18:23:33 +0300

    init

---
+++ src/main/java/ru/korolyovegor/LearnUp_Java_Course_hw_20/LearnUpJavaCourseHw20Application.java
@@ -0,0 +1,13 @@
+package ru.korolyovegor.LearnUp_Java_Course_hw_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnUpJavaCourseHw20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnUpJavaCourseHw20Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/korolyovegor/LearnUp_Java_Course_hw_20/LearnUpJavaCourseHw20ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.korolyovegor.LearnUp_Java_Course_hw_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnUpJavaCourseHw20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
