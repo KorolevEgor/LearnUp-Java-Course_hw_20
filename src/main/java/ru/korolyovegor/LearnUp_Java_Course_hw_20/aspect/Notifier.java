3faeaf7e28bc6b463e30af12823194aa165e260c
KorolevEgor <korolevegir@gmail.com>
2022-04-23 19:35:20 +0300

    Create Notifier.java

---
+++ src/main/java/ru/korolyovegor/LearnUp_Java_Course_hw_20/aspect/Notifier.java
@@ -0,0 +1,10 @@
+package ru.korolyovegor.LearnUp_Java_Course_hw_20.aspect;
+
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Aspect;
+
+@Component
+@Aspect
+public class Notifier {
+
+}
